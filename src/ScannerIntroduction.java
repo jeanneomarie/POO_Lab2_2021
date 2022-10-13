1c3f2f310419feb3021dd9e134680ab80a4afc12
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-13 23:01:21 +0300

    Update ScannerIntroduction.java

--- src/ScannerIntroduction.java
+++ src/ScannerIntroduction.java
@@ -4,9 +4,11 @@ public class ScannerIntroduction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("What is your name?");
         String name = scanner.nextLine();
         System.out.println("My name is " + name);
 
+        System.out.println("How old are you?");
         int age = scanner.nextInt();
         System.out.println("My age is " + age);
 
