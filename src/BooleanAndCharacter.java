b9ba3af9da56074cdc4552144b2361fd031a5110
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-13 23:00:20 +0300

    Update BooleanAndCharacter.java

--- src/BooleanAndCharacter.java
+++ src/BooleanAndCharacter.java
@@ -2,6 +2,8 @@ import java.math.BigInteger;
 
 public class BooleanAndCharacter {
     public static void main(String[] args) {
+        System.out.println(10 == 10);
+
         BigInteger myBigInteger = new BigInteger("10");
         System.out.println(myBigInteger instanceof BigInteger);
 
