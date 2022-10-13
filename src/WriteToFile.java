448ef47be9254611e8a0a55692bffd4d5b9ce361
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-13 23:02:02 +0300

    Update WriteToFile.java

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -4,13 +4,14 @@ public class WriteToFile {
     public static void main(String[] args) {
         try {
             FileWriter writer = new FileWriter("file.txt");
+            
             writer.write("Hello");
             writer.write("\r\n");
             writer.write("Bye");
+            
             writer.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
     }
 }
