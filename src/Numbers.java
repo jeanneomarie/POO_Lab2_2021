58e6a495c8d1578d2e063fe9097af40154a5e842
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-13 22:58:02 +0300

    Update Numbers.java

--- src/Numbers.java
+++ src/Numbers.java
@@ -9,29 +9,23 @@ public class Numbers {
         long myLong = 10L;
         System.out.println(myLong);
 
-        Integer myInteger = 10;
-        //Integer myInteger = Integer.valueOf("10");
-        //String myIntegerString = myInteger;
-        String myIntegerString2 = myInteger.toString();
-        System.out.println(myIntegerString2);
+        int myHexadecimal = 0x2A;
+        System.out.println(myHexadecimal);
+
+        double myDouble = 3.14;
+        System.out.println(myDouble);
+
+        //float myFloat = 3.14; //show the error of incompatibility => STRONG TYPED LANGUAGE
+        float myFloat = 3.14f;
+        System.out.println(myFloat);
+
+        double positiveInfinity = Double.POSITIVE_INFINITY;
+        double negativeInfinity = Double.NEGATIVE_INFINITY;
+        System.out.println(positiveInfinity+positiveInfinity);
 
         BigInteger myBigInteger1 = new BigInteger("1");
         BigInteger myBigInteger2 = new BigInteger("2");
         BigInteger myBigIntegerSum = myBigInteger1.add(myBigInteger2);
         System.out.println(myBigIntegerSum);
-
-        int myHexadecimal = 0x2A;
-        System.out.println(myHexadecimal);
-
-        //float myFloat = 3.14;
-        float myFloat = 3.14f;
-        System.out.println(myFloat);
-
-        double myDouble = 3.14;
-        System.out.println(myDouble);
-
-        double positiveInfinity = Double.POSITIVE_INFINITY;
-        double negativeInfinity = Double.NEGATIVE_INFINITY;
-        System.out.println(positiveInfinity+positiveInfinity);
     }
 }
